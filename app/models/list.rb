4aa7fce5f64f1a23ef7ec4542da4864a7802f7c9
lowpez <lopezaguayo@aol.com>
2021-08-01 01:57:50 -0400

    Watch List w/ Images listo

--- app/models/list.rb
+++ app/models/list.rb
@@ -4,4 +4,11 @@ class List < ApplicationRecord
   has_many :reviews
 
   validates :name, uniqueness: true, presence: true
+  has_one_attached :photo
+
+  # validates :photo, format: {
+  # with: %r{\.(png|jpg|jpeg)\Z}i,
+  # message: 'must be a url for gif, jpg, or png image.'
+  # }
+
 end
